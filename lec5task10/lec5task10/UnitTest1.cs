7442577b257b356e71af0875f71073ca36c27329
AKartashov <AKartashov@AKARTASHOV>
2018-05-10 12:59:13 +0300

    fix code

--- lec5task10/lec5task10/UnitTest1.cs
+++ lec5task10/lec5task10/UnitTest1.cs
@@ -15,17 +15,7 @@ namespace lec5task10
         static IWebDriver driver;
         static IWebDriver f_driver;
 
-        public string[] color_value(IWebDriver driver, string xp_)
-        {
-            string orig_price_norm_c = driver.FindElement(By.XPath(xp_)).GetCssValue("color");
-            List<string> colors = new List<string>();
-            foreach (Match m in Regex.Matches(orig_price_norm_c, @"\d{1,3}"))
-            {
-                colors.Add(m.Value);
-            }
-            return colors.ToArray();
-        }
-        public string[] color_value(IWebDriver driver, IWebElement el, string xp_)
+        public string[] color_value(IWebElement el, string xp_)
         {
             string orig_price_norm_c = el.FindElement(By.XPath(xp_)).GetCssValue("color");
             List<string> colors = new List<string>();
@@ -36,72 +26,76 @@ namespace lec5task10
             return colors.ToArray();
         }
 
+        public void checkRedColor(string[] colors)
+        {
+            Assert.IsTrue((Int32.Parse(colors[0]) > Int32.Parse(colors[1])) && (Int32.Parse(colors[0]) > Int32.Parse(colors[2])), "Price is not red!");
+        }
+
+        public  void checkGrayColor(string[] colors)
+        {
+            Assert.IsTrue(colors[0].Equals(colors[1]) && colors[1].Equals(colors[2]), "Price is not gray!");
+        }
+
+        public void checkBold(int bold_value)
+        {
+            Assert.IsTrue(bold_value >= 700, "Price is not bold");
+        }
+
+        public void checkCross(string atribute)
+        {
+            Assert.IsTrue(atribute.Equals("line-through"), "Price is not crossed");
+        }
+
+        public void compareFontSizes(IWebElement element)
+        {
+            string new_pr_s_fs = element.FindElement(By.XPath(".//*[@class='campaign-price']")).GetCssValue("font-size");
+            new_pr_s_fs = new_pr_s_fs.Substring(0, 2);
+            string new_pr_n_fs = element.FindElement(By.XPath(".//*[@class='regular-price']")).GetCssValue("font-size");
+            new_pr_n_fs = new_pr_n_fs.Substring(0, 2);
+            Assert.IsFalse(Int32.Parse(new_pr_n_fs) >= Int32.Parse(new_pr_s_fs), "Wrong font size of proces!");
+        }
         public void test_prices(IWebDriver driver)
         {
             driver.Navigate().GoToUrl("http://localhost/litecart/");
             IWebElement duck = driver.FindElement(By.XPath("//div[@id='box-campaigns']//li"));
 
-            // a) на главной странице и на странице товара совпадает текст названия товара
             string orig_name = duck.FindElement(By.XPath(".//div[@class='name']")).Text;
-            // б) на главной странице и на странице товара совпадают цены(обычная и акционная)
             string orig_price_norm = duck.FindElement(By.XPath(".//*[@class='regular-price']")).Text;
             string orig_price_sale = duck.FindElement(By.XPath(".//*[@class='campaign-price']")).Text;
 
-            // Проверка серого цвета цены
-            string[] orig_pr_n_c = color_value(driver,duck, ".//*[@class='regular-price']");
-            Assert.IsTrue(orig_pr_n_c[0].Equals(orig_pr_n_c[1]) && orig_pr_n_c[1].Equals(orig_pr_n_c[2]), "Orig price is not gray!");
-
-            // Проверка красного цвета акцонной цены
-            string[] orig_pr_s_c = color_value(driver,duck, ".//*[@class='campaign-price']");
-            Assert.IsTrue((Int32.Parse(orig_pr_s_c[0]) > Int32.Parse(orig_pr_s_c[1])) && (Int32.Parse(orig_pr_s_c[0]) > Int32.Parse(orig_pr_s_c[2])), "Sale price is not red!");
-
-            // Проверка жирности шрифта акционной цены
+            string[] orig_pr_n_c = color_value(duck, ".//*[@class='regular-price']");
+            string[] orig_pr_s_c = color_value(duck, ".//*[@class='campaign-price']");
             int orig_pr_s_bl = Int32.Parse(duck.FindElement(By.XPath(".//*[@class='campaign-price']")).GetCssValue("font-weight"));
-            Assert.IsTrue(orig_pr_s_bl >= 700, "Sale price is not bold");
-
-            // Проверка перечеркнутости цены
             string orig_pr_n_dec = duck.FindElement(By.XPath(".//*[@class='regular-price']")).GetCssValue("text-decoration-line");
-            Assert.IsTrue(orig_pr_n_dec.Equals("line-through"), "Price is not crossed");
 
-            // Шрифт акционной цены должен быть больше
-            string orig_pr_s_fs = duck.FindElement(By.XPath(".//*[@class='campaign-price']")).GetCssValue("font-size");
-            orig_pr_s_fs = orig_pr_s_fs.Substring(0, 2);
-            string orig_pr_n_fs = duck.FindElement(By.XPath(".//*[@class='regular-price']")).GetCssValue("font-size");
-            orig_pr_n_fs = orig_pr_n_fs.Substring(0, 2);
-            Assert.IsTrue(Int32.Parse(orig_pr_n_fs) < Int32.Parse(orig_pr_s_fs), "Wrong font size of proces!");
+            checkGrayColor(orig_pr_n_c);  
+            checkRedColor(orig_pr_s_c);
+            checkBold(orig_pr_s_bl);
+            checkCross(orig_pr_n_dec);
+            compareFontSizes(duck);
 
             // На новой странице
             duck.FindElement(By.XPath("./a")).Click();
-            string new_name = driver.FindElement(By.XPath("//h1[@class='title']")).Text;
-            string new_price_norm = driver.FindElement(By.XPath("//*[@class='regular-price']")).Text;
-            string new_price_sale = driver.FindElement(By.XPath("//*[@class='campaign-price']")).Text;
+            IWebElement new_duck = driver.FindElement(By.XPath("//div[@id='box-product']"));
+
+            string new_name = new_duck.FindElement(By.XPath(".//h1[@class='title']")).Text;
+            string new_price_norm = new_duck.FindElement(By.XPath(".//*[@class='regular-price']")).Text;
+            string new_price_sale = new_duck.FindElement(By.XPath(".//*[@class='campaign-price']")).Text;
+
+            string[] new_pr_n_c = color_value(new_duck, ".//*[@class='regular-price']");
+            string[] new_pr_s_c = color_value(new_duck, ".//*[@class='campaign-price']");
+            int new_pr_s_bl = Int32.Parse(new_duck.FindElement(By.XPath(".//*[@class='campaign-price']")).GetCssValue("font-weight"));
+            string new_pr_n_dec = new_duck.FindElement(By.XPath(".//*[@class='regular-price']")).GetCssValue("text-decoration-line");
 
             Assert.IsTrue(new_name.Equals(orig_name), "Names are different");
             Assert.IsTrue(new_price_norm.Equals(orig_price_norm), "Prices are different");
             Assert.IsTrue(new_price_sale.Equals(orig_price_sale), "Sale prices are different");
 
-            // Проверка серого цвета акционной цены
-            string[] new_pr_n_c = color_value(driver,"//*[@class='regular-price']");
-            Assert.IsTrue(new_pr_n_c[0].Equals(new_pr_n_c[1]) && new_pr_n_c[1].Equals(new_pr_n_c[2]), "Orig price is not gray!");
-
-            // Проверка красного цвета акционной цены
-            string[] new_pr_s_c = color_value(driver,"//*[@class='campaign-price']");
-            Assert.IsTrue((Int32.Parse(new_pr_s_c[0])> Int32.Parse(new_pr_s_c[1])) && (Int32.Parse(new_pr_s_c[0]) > Int32.Parse(new_pr_s_c[2])), "Sale price is not red!");
-
-            // Проверка жирности шрифта акционной цены
-            int new_pr_s_bl = Int32.Parse(driver.FindElement(By.XPath("//*[@class='campaign-price']")).GetCssValue("font-weight"));
-            Assert.IsTrue(new_pr_s_bl >= 700, "Sale price is not bold");
-
-            // Проверка перечеркнутости цены
-            string new_pr_n_dec = driver.FindElement(By.XPath("//*[@class='regular-price']")).GetCssValue("text-decoration-line");
-            Assert.IsTrue(new_pr_n_dec.Equals("line-through"), "Price is not crossed");
-
-            // Шрифт акционной цены должен быть больше
-            string new_pr_s_fs = driver.FindElement(By.XPath("//*[@class='campaign-price']")).GetCssValue("font-size");
-            new_pr_s_fs = new_pr_s_fs.Substring(0, 2);
-            string new_pr_n_fs = driver.FindElement(By.XPath("//*[@class='regular-price']")).GetCssValue("font-size");
-            new_pr_n_fs = new_pr_n_fs.Substring(0, 2);
-            Assert.IsFalse(Int32.Parse(new_pr_n_fs) >= Int32.Parse(new_pr_s_fs), "Wrong font size of proces!");
+            checkGrayColor(new_pr_n_c);            
+            checkRedColor(new_pr_s_c);
+            checkBold(new_pr_s_bl);
+            checkCross(new_pr_n_dec);
+            compareFontSizes(new_duck);
 
             driver.Quit();
         }
